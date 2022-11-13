10245ce367903599a2ea5e6b2d99e1966765e04f
JulianDBonillaF <116658293+JulianDBonillaF@users.noreply.github.com>
2022-11-13 18:30:54 -0500

    bullet box collider

--- Sentinel Expedition/Assets/Scripts/SpawnManager.cs
+++ Sentinel Expedition/Assets/Scripts/SpawnManager.cs
@@ -16,7 +16,7 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        numberEnemy = FindObjectsOfType<Enemy>().Length;
+        numberEnemy = FindObjectsOfType<EnemyFinding1>().Length;
         if (numberEnemy == 0)
         {
             numberAttack++;
