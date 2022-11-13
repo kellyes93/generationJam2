13c842056bf3bd480d4d5fca23211de05e246d61
Juan P. Frasser <frazzer_1004@hotmail.com>
2022-11-13 02:36:49 -0500

    se arregla bug de sobre dimencionamiento de esposa al iniciar la escena

--- Sentinel Expedition/Assets/Scripts/FollowThePlayer.cs
+++ Sentinel Expedition/Assets/Scripts/FollowThePlayer.cs
@@ -26,13 +26,13 @@ public class FollowThePlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        TargetFollow ();
-        FlipSprite ();
+        TargetFollow();
+        // FlipSprite ();
         animator.SetFloat("Horizontal", Mathf.Abs(playerController.move));
         animator.SetFloat("Vertical", Mathf.Abs(playerController.movey));
     }
 
-    void TargetFollow ()
+    void TargetFollow()
     {
         if (Vector2.Distance(transform.position, target.position) > targetPosition)
         {
@@ -40,13 +40,13 @@ public class FollowThePlayer : MonoBehaviour
         }
     }
 
-    void FlipSprite ()
+    void FlipSprite()
     {
         if (playerTransform.position.x > transform.position.x)
         {
             //Turn face right
             transform.localScale = new Vector3(1, 1, 1);
-        } 
+        }
         else if (playerTransform.position.x < transform.position.x)
         {
             //Turn face left
