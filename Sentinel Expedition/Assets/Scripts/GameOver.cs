eee19a321cb59cb2e151f3e37b95d83ff9f46152
Juan P. Frasser <frazzer_1004@hotmail.com>
2022-11-15 11:13:14 -0500

    game over setted

--- Sentinel Expedition/Assets/Scripts/GameOver.cs
+++ Sentinel Expedition/Assets/Scripts/GameOver.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using System;
 
 public class GameOver : MonoBehaviour
 {
+    public GameObject gameOverMenu;
+    private PlayerDiedAnimation died;
     public AudioSource clip;
     // Start is called before the first frame update
     void Start()
     {
-
+        died = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDiedAnimation>();
+        died.overHandler += ActivateCanvas;
     }
 
     // Update is called once per frame
@@ -28,6 +32,10 @@ public class GameOver : MonoBehaviour
 
         clip.Play();
         SceneManager.LoadScene(nameLevel);
+    }
+    private void ActivateCanvas(object sender, EventArgs e)
+    {
+        gameOverMenu.SetActive(true);
     }
 
 }
--- Sentinel Expedition/Assets/Scripts/Player/PlayerDiedAnimation.cs
+++ Sentinel Expedition/Assets/Scripts/Player/PlayerDiedAnimation.cs
@@ -1,28 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerDiedAnimation : MonoBehaviour
 {
+    public event EventHandler overHandler;
     public float pointsLifePlayer;
-    public GameObject diedEffect; 
+    public GameObject diedEffect;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void TakeDamage (float damage)
+    public void TakeDamage(float damage)
     {
         pointsLifePlayer -= damage;
         if (pointsLifePlayer <= 0)
         {
+            overHandler?.Invoke(this, EventArgs.Empty);
             Died();
         }
     }
@@ -31,6 +34,6 @@ public class PlayerDiedAnimation : MonoBehaviour
     {
         Instantiate(diedEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
-    }  
+    }
 
 }
